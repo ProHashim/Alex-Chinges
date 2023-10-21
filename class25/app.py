17765d20aed37d88d491d1b22493bcaf0aa1ebdb
prohashim <prohashim4e@gmail.com>
2023-10-21 15:15:13 +0500

    25 session

---
+++ class25/app.py
@@ -0,0 +1,11 @@
+class Person:
+    print("Helloooo OOP")
+    # property
+    mobile = input("What cell phone are you using?")
+
+
+# alex = Person()
+# print(alex.mobile)
+
+hashim = Person()
+print(hashim.mobile)
