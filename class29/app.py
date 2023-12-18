a661f1020a3715d9c73cc619fb5492d98efd951e
prohashim <prohashim4e@gmail.com>
2023-12-18 16:48:37 +0500

    class 29

---
+++ class29/app.py
@@ -0,0 +1,36 @@
+# Write code that will take two numbers and add them.
+
+# First way of adding two numbers
+print(4 + 5)
+
+# second way of adding two numbers
+number1 = 56
+number2 = 67
+sum_of_numbers = number1 + number2
+print (sum_of_numbers)
+
+# Variable: named memory location that may or may not have a value
+
+avalue =""
+print (avalue)
+
+
+# add two numbers using function:
+def name():
+    pass
+
+name()
+
+# parameters
+def add_two_values(value1, value2):
+    # return value1 + value2
+    print (value1 + value2)
+# arguments
+add_two_values(63, 2)
+
+def input_from_user():
+    num1 = int(input("Number1: "))
+    num2 = int(input("Number2: "))
+    print("The number is: ", num1  + num2)
+
+input_from_user()
